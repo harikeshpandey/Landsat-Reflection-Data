cc376fd184fa65228aa64ef2991612664d284299
Rachit-Pandey-2004 <rachit_pandey2004@yahoo.com>
2024-10-09 09:42:08 +0530

    fixed map view

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,25 +2,25 @@ import Navbar from "../componenets/Navbar";
 import MapView from "../componenets/Map";
 import Footer from "../componenets/Footer";
 
-
-import React from 'react'
-
-
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <div className="relative h-screen w-screen overflow-hidden">
-      <div id="map" className="absolute inset-0 z-0">
-        <MapView/>
+    <div className="flex flex-col h-screen overflow-hidden">
+      {/* Navbar fixed at the top */}
+      <div className="z-20">
+        <Navbar />
       </div>
-      <div className="relative z-10 ">
-        <Navbar/>
+
+      {/* MapView takes up remaining space */}
+      <div className="flex-grow z-0">
+        <MapView />
       </div>
+
+      {/* Footer fixed at the bottom */}
+      <Footer />
     </div>
-      <Footer/>
-    </div>
-  )
-}
+  );
+};
 
 export default Home;
